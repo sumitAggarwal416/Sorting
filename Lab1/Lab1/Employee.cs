1f58497c5dcf3738c34a696163a370cd907dc1ae
Sumit Aggarwal <55593642+sumitAggarwal416@users.noreply.github.com>
2019-10-26 22:06:31 -0400

    Add files via upload

---
+++ Lab1/Lab1/Employee.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * @author Sumit Aggarwal - 000793607
+ * I certify that all work done in the assignment is my own work and that I have not copied it
+ * from any other source. I also certify that I have not allowed anyone else to copy my code.
+ * 
+ */
+using System;
+
+namespace Lab1
+{
+    class Employee
+    {
+        private string name;
+        private int number;
+        private decimal rate;
+        private double hours;
+        private decimal gross;
+
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+            this.name = name; // sets the current value of name at this instance when the 
+            //constructor is called
+            this.number = number; // sets the current value of number at this instance when the
+            // constructor is called
+            this.rate = rate; // sets the current value of rate at this instance when the 
+            // constructor is called
+            this.hours = hours; // sets the current value of hours at this instance when the 
+            // cunstroctor is called
+        }
+
+        public decimal GetGross()
+        { // this method is used to obtain the value of the gross pay of the employee
+            //if he/she is working for more than 40 hours, they will be getting overtime pay
+            if (hours <= 40 && hours > 0)
+            {
+                gross = rate * (decimal)hours; // variable hours (double) is type-casted to 
+                // decimal because we cannot multiply a double and a decimal value
+            }
+            else
+                gross = rate * (decimal)hours;
+                
+            return gross;
+        }
+
+        public double GetHours()
+        { // this method is used to obtain the number of hours an employee has worked
+            return hours;
+        }
+
+        public string GetName()
+        { //  // this method is used to obtain the name of an employee
+            return name;
+        }
+
+        public int GetNumber()
+        {  // this method is used to obtain the employee number of an employee
+            return number;
+        }
+
+        public decimal GetRate()
+        {  // this method is used to obtain the hourly wage of an employee
+            return rate;
+        }
+
+        public override string ToString()
+        {
+            return "["+name+","+number+","+rate+","+hours+"]";
+        }
+
+        public void SetHours(double hours)
+        { // used to set the value of hours at the current instance
+            this.hours = hours;
+        }
+
+        public void SetName(string name)
+        { // used to set name of an employee at the current instance
+            this.name = name;
+        }
+
+        public void SetNumber(int number)
+        { // used to set the number of an employee at the current instance
+            this.number = number;
+        }
+
+        public void SetRate(decimal rate)
+        { // used to set the hourly wage of an employee at the current instance
+            this.rate = rate;
+        }
+
+    }
+
+}
+    
+
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,160 @@
+﻿/*
+ * @author Sumit Aggarwal - 000793607
+ * I certify that all work done in the assignment is my own work and that I have not copied it
+ * from any other source. I also certify that I have not allowed anyone else to copy my code.
+ * 
+ * Reference -  https://www.geeksforgeeks.org/selection-sort/
+ * 
+ */
+using System;
+using System.IO;
+
+namespace Lab1
+{
+    class Program
+    {
+        private static int option;
+
+        static void Main(String[] args)
+        {
+            Employee[] employees = new Employee[100]; // array of the type Employee with the
+            // maximum size of 100 employees
+            employees = Read(employees); // calls the function Read() to read the employees' data
+
+            bool isRunning = true; // loop control variable
+            while (isRunning)
+            {
+                Console.WriteLine("----------------H O M E ----------------");
+                Console.WriteLine("Sort by: \n" +
+                    "1 - Employee name \n" +
+                    "2 - Employee number \n" +
+                    "3 - Pay rate\n" +
+                    "4 - Employee hours\n" +
+                    "5 - Gross Pay\n" +
+                    "6 - Exit\n\n");
+
+                option = int.Parse(Console.ReadLine()); // the option user chooses to sort the
+                // data with
+
+                switch (option)
+                {
+                    case 1:
+                        sort(employees);
+                        break;
+                    case 2:
+                        sort(employees);
+                        break;
+                    case 3:
+                        sort(employees);
+                        break;
+                    case 4:
+                        sort(employees);
+                        break;
+                    case 5:
+                        sort(employees);
+                        break;
+                    case 6:
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please re-enter!");
+                        break;
+                }
+            }
+        }
+
+        /*
+         * @param Employee[] array - array that will be passed in the function to get sorted
+         * returns void
+         */
+        static void sort(Employee[] array)
+        { // Reference - https://www.geeksforgeeks.org/selection-sort/
+            int n = array.Length; // the length of the array that is passed as argument
+
+            for (int i = 0; i < n-1 ; i++) // outer loop
+            {
+                int min_index = i; // the index of the value with the smallest which will be 
+                // swapped to a "upper" position and the larger one in its place
+                for (int j = i+1; j < n; j++) // inner loop
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            if (array[j].GetName().CompareTo(array[min_index].GetName()) < 0)
+                            {
+                                min_index = j;
+                            }
+                            break;
+                        case 2:
+                            if (array[j].GetNumber() < array[min_index].GetNumber())
+                                min_index = j;
+                            break;
+                        case 3:
+                            if (array[j].GetRate() > array[min_index].GetRate())
+                                min_index = j;
+                            break;
+                        case 4:
+                            if (array[j].GetHours() > array[min_index].GetHours())
+                                min_index = j;
+                            break;
+                        case 5:
+                            if (array[j].GetGross() > array[min_index].GetGross())
+                                min_index = j;
+                            break;
+                    }
+                    
+                }
+                // swapping the smaller values with the larger ones
+                Employee temp = array[min_index];
+                array[min_index] = array[i];
+                array[i] = temp;
+            }
+            for (int i = 0; i < n ; i++)
+            {
+                Console.WriteLine(array[i] + " "); //prints the elements of the array
+            }
+            Console.WriteLine();
+        }
+
+        /*
+         * @param Employee[] array - array that will be passed in the function to get sorted
+         * returns an array of the type Employee
+         */
+        static Employee[] Read(Employee[] array)
+        {
+            string line = "";
+            int lineNumber = 0;
+
+            try
+            {
+                StreamReader fileInput = new StreamReader("employees.csv"); // opens the file to 
+                // read
+                lineNumber = 0;
+                while (!fileInput.EndOfStream)
+                {
+                    line = fileInput.ReadLine();
+                    string[] parts = line.Split(',');
+                    array[lineNumber] = new Employee(parts[0], int.Parse(parts[1]),
+                        decimal.Parse(parts[2]), double.Parse(parts[3]));
+                    lineNumber++;
+                }
+                fileInput.Close(); // close the file i.e. stop reading the file
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Could not find file 'employees.csv'" + ex.Message);
+           
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Error on line {lineNumber + 1} reading line{line}" +
+                    $"- {ex.Message}");
+                
+            }
+            // trim the size of the array to the number of values in the file
+            Employee[] newEmployees = new Employee[lineNumber];
+            Array.ConstrainedCopy(array, 0, newEmployees, 0, lineNumber);
+            return newEmployees;
+        }
+    }
+}
